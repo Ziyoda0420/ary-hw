29561ef80ded57f31a8d115db16769f45e5a4a9e
Ziyoda0420 <mirzakirovaziyoda@gmail.com>
2025-01-25 14:35:33 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+let people =[ 'olma' ,'banan','uzum','nok'];
+console.log(people);
+
+let user = (`Boshlanish arryning uzunligi 3`);
+alert(user);
+
+let numer = 'Siz  boshlanish arry malumotlarini olib tawlamoqchimisiz?';
+alert(numer);
+
+console.log(people.shift())
+console.log(people);
+let numel ="Arry soni 2";
+alert(numel)
+
+
+
